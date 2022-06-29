d87787144e56dadc6ea5dbd3d37731f754c2adb0
jukebox <jukebox@jukebox.ltd>
2022-06-29 12:38:54 +1000

    Adds hello world plugin
    Co-authored-by: yx <isaac.k.masood@gmail.com>

---
+++ src/plugins/test.cs
@@ -0,0 +1,13 @@
+namespace Oxide.Plugins
+{
+    [Info("Skills", "jukebox, yx", "0.0.1")]
+    [Description("Adds skills and skill training to Rust.")]
+    public class Skills : RustPlugin
+    {
+        void Init()
+        {
+            Puts("Hello world!");
+            return;
+        }
+    }
+}
